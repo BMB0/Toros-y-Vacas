df2f401f37c671c10f5d04c8eeee46cdddc7bd83
BMB <bruno.marquez@ucb.edu.bo>
2022-04-10 18:35:28 -0400

    Se agrega que devulva ! si el primer numero coincide en posicion y valor

--- src/toros_vacas.js
+++ src/toros_vacas.js
@@ -2,6 +2,10 @@ function juego(SecretNumber, GuessNumber) {
   var res = "";
   if(SecretNumber === GuessNumber) {
     res = "!!!!";
+  }else{
+    if(SecretNumber[0] === GuessNumber[0]){
+      res += "!";
+    }
   }
   return res;
 }
--- src/toros_vacas.test.js
+++ src/toros_vacas.test.js
@@ -2,9 +2,12 @@ import juego from "./toros_vacas.js";
 
 describe("Sumar", () => {
   it("Devuelve una cadena vacia al ingresar un numero que no coincide ni en posicion ni valor", () => {
-    expect(juego(1234, 5678)).toEqual("");
+    expect(juego("1234", "5678")).toEqual("");
   });
-  it("Devuelve !!!! al ingresar un numero que coincide en posicion y valor", () => {
-    expect(juego(1234, 1234)).toEqual("!!!!");
+  it("Devuelve !!!! al ingresar una cadena de numeros que coincidan en posicion y valor", () => {
+    expect(juego("1234", "1234")).toEqual("!!!!");
+  });
+  it("Devuelve ! al ingresar un numero que coincide en posicion y valor", () => {
+    expect(juego("1234", "1567")).toEqual("!");
   });
 });
