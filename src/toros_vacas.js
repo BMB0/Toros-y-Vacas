a03a206dbb5f31a6e6aacb84175be7f32e655e24
BMB <bruno.marquez@ucb.edu.bo>
2022-04-10 18:44:27 -0400

    Se hace una refactorizacion

--- src/toros_vacas.js
+++ src/toros_vacas.js
@@ -1,20 +1,16 @@
 function juego(SecretNumber, GuessNumber) {
   var res = "";
-  if(SecretNumber === GuessNumber) {
-    res = "!!!!";
-  }else{
-    if(SecretNumber[0] === GuessNumber[0]){
-      res = "!";
-    }
-    if(SecretNumber[1] === GuessNumber[1]){
-      res = "!";
-    }
-    if(SecretNumber[2] === GuessNumber[2]){
-      res = "!";
-    }
-    if(SecretNumber[3] === GuessNumber[3]){
-      res = "!";
-    }
+  if(SecretNumber[0] === GuessNumber[0]){
+    res += "!";
+  }
+  if(SecretNumber[1] === GuessNumber[1]){
+    res += "!";
+  }
+  if(SecretNumber[2] === GuessNumber[2]){
+    res += "!";
+  }
+  if(SecretNumber[3] === GuessNumber[3]){
+    res += "!";
   }
   return res;
 }
