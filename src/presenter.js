0b90b4f4f94860c92a4f7fb62b4140a44fb75356
BMB <bruno.marquez@ucb.edu.bo>
2022-04-10 20:20:35 -0400

    Agregado ingresar codigo secreto y realizar intentos

--- src/presenter.js
+++ src/presenter.js
@@ -1,14 +1,20 @@
 import juego from "./toros_vacas.js";
 
-const first = document.querySelector("#numero-secreto");
+const numSecret = document.querySelector("#numero-secreto");
 const form = document.querySelector("#toros-vacas-form");
-const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+});
 
-  const firstNumber = Number.parseInt(first.value);
+const guess = document.querySelector("#numero-intento");
+const form2 = document.querySelector("#intento-form");
+const div = document.querySelector("#resultado-div");
 
-  div.innerHTML = "<p>" + firstNumber + "</p>";
-  //div.innerHTML = "<p>" + juego(firstNumber, secondNumber) + "</p>";
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+  var secretNumber = numSecret.value
+  var guessNumber = guess.value
+  //div.innerHTML = "<p>"+secretNumber +"-----" + guessNumber +"-----"+juego(secretNumber, guessNumber) + "</p>";
+  div.innerHTML = "<p>" + juego(secretNumber, guessNumber) + "</p>";
 });
