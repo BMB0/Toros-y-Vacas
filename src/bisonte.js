3fde7b9bb83097ed8f29f8ae7699a58025a5415f
Bruno Marquez <bruno.marquez@ucb.edu.bo>
2022-04-12 12:09:06 -0400

    Se aplica una refactorizacion en bisonte

--- src/bisonte.js
+++ src/bisonte.js
@@ -1,21 +1,13 @@
 function bisonte (SecretNumber) {
     var bisonte ="";
-    var numero = parseInt(SecretNumber[0]);
-    if(numero / 2 == 1 || numero / 3 == 1 || numero / 5 == 1 || numero / 7 == 1) {
-        bisonte += "%";
+    
+    for(var i = 0; i < 4; i++) {
+        var numero = parseInt(SecretNumber[i]);
+        if(numero / 2 == 1 || numero / 3 == 1 || numero / 5 == 1 || numero / 7 == 1) {
+            bisonte += "%";
+        }
     }
-    var numero = parseInt(SecretNumber[1]);
-    if(numero / 2 == 1 || numero / 3 == 1 || numero % 5 == 1 || numero / 7 == 1) {
-        bisonte += "%";
-    }
-    var numero = parseInt(SecretNumber[2]);
-    if(numero / 2 == 1 || numero / 3 == 1 || numero % 5 == 1 || numero / 7 == 1) {
-        bisonte += "%";
-    }
-    var numero = parseInt(SecretNumber[3]);
-    if(numero / 2 == 1 || numero / 3 == 1 || numero % 5 == 1 || numero / 7 == 1) {
-        bisonte += "%";
-    }
+    
     return bisonte;
 }
 
